9bd41bc08727ff98466a11514c7fcf6b9861486a
MaxSkippGriff <60075283+MaxSkippGriff@users.noreply.github.com>
2021-06-02 13:51:08 +0100

    :tada: Initial commit for ResNet-50

---
+++ main.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+
+class block(nn.Module):
+    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+        super(block, self).__init__()
+        self.expansion = 4
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        # Same number of channels
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, stride=1, padding=0)
+        self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
+        self.relu = nn.RELU()
+        # Conv layer to identity mapping
+        self.identity_downsample = identity_downsample
+
+    # Go through each layer defined
+    def forward(self, x):
+        identity = x
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        # We use identity downsample layer if
+        # we need to change the shape
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+        
+        x += identity
+        x = self.relu(x)
+        return x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
